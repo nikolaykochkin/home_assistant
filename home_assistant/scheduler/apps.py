6f6b83d83ab2f40017f48dc249d0f7167266b942
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-09-19 22:40:50 +0300

    models done

--- home_assistant/scheduler/apps.py
+++ home_assistant/scheduler/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SchedulerConfig(AppConfig):
     name = 'scheduler'
+    verbose_name = "планировщик"
--- home_assistant/scheduler/models.py
+++ home_assistant/scheduler/models.py
@@ -1,5 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=200, verbose_name='наименование', db_index=True)
+    executor = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='исполнитель', related_name='+', db_index=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='+', verbose_name='автор')
+    description = models.TextField(verbose_name='описание', blank=True)
+    active = models.BooleanField(verbose_name='активно', default=True)
+    start_date = models.DateTimeField(verbose_name='дата начала')
+    end_date = models.DateTimeField(
+        verbose_name='дата окончания', null=True, blank=True)
+    start_time = models.TimeField(
+        verbose_name='время начала', null=True, blank=True)
+    everyday = models.BooleanField(verbose_name='каждый день')
+    day_of_month = models.IntegerField(
+        verbose_name='день месяца',
+        blank=True,
+        null=True,
+        validators=[MinValueValidator(0), MaxValueValidator(31)])
+    monday = models.BooleanField(verbose_name='понедельник')
+    tuesday = models.BooleanField(verbose_name='вторник')
+    wednesday = models.BooleanField(verbose_name='среда')
+    thursday = models.BooleanField(verbose_name='четверг')
+    friday = models.BooleanField(verbose_name='пятница')
+    saturday = models.BooleanField(verbose_name='суббота')
+    sunday = models.BooleanField(verbose_name='воскресенье')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'задача'
+        verbose_name_plural = 'задачи'
